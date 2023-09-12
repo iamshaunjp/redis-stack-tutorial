4d5355605f3f38eacdb2a003e0a22fab5668fa6f
Shaun <shaun@thenetninja.co.uk>
2023-09-12 13:52:57 +0100

    lesson-13

--- src/app/_actions/products.js
+++ src/app/_actions/products.js
@@ -1,10 +1,21 @@
 'use server'
 
 import { redirect } from "next/navigation"
+import { productRepository } from "../_schema/product"
 
 // create products
 export async function createProduct(formData) {
+  
+  await productRepository.save({
+    title: formData.get('title'),
+    count: parseInt(formData.get('count')),
+    price: parseFloat(formData.get('price')),
+    img: formData.get('img'),
+    colors: formData.getAll('colors'),
+    size: formData.get('size')
+  })
 
+  redirect('/products')
 }
 
 // update products
