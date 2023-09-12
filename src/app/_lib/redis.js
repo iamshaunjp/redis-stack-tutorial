945d1bf97b79164648cf768dd967af374a69e04d
Shaun <shaun@thenetninja.co.uk>
2023-09-12 14:04:04 +0100

    lesson-12

--- src/app/_lib/redis.js
+++ src/app/_lib/redis.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis'
 
 const client = createClient({
-  password: process.env.REDIS_PW,
+  password: process.env.REDIS_PASSWORD,
   socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT
